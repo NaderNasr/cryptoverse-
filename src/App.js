cbd831ec86f188a496ba3c702fa7c820cbc8820d
NaderNasr <nadernasr7@gmail.com>
2022-06-06 11:25:50 -0400

    app layout init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route, Link } from 'react-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './components/componentLib';
+
 
 const App = () => {
   return (
-    <div>
-      Hello World!
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+
+      </div>
+      <div className='footer'>
+
+      </div>
     </div>
   )
 }

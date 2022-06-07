565cdcbf44b6263f502e41f474db69ef03d00643
NaderNasr <nadernasr7@gmail.com>
2022-06-07 18:39:44 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar, Exchanges, Homepage, Currencies, News, Details } from './components/lib/componentLib';
 
-import './App.css'
+// import './App.css'
 
 
 const App = () => {
@@ -16,17 +16,27 @@ const App = () => {
         <Layout>
           <div className='routes'>
             <Routes>
-              <Route exact path='/' element={<Homepage />}/>
-              <Route exact path='/exchanges' element={<Exchanges />}/>
-              <Route exact path='/currencies' element={<Currencies />}/>
-              <Route exact path='/details/:coinId' element={<Details />}/>
-              <Route exact path='/news' element={<News />}/>
+              <Route exact path='/' element={<Homepage />} />
+              <Route exact path='/exchanges' element={<Exchanges />} />
+              <Route exact path='/currencies' element={<Currencies />} />
+              <Route exact path='/details/:coinId' element={<Details />} />
+              <Route exact path='/news' element={<News />} />
             </Routes>
           </div>
         </Layout>
-      </div>
-      <div className='footer'>
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            CryptoVerse <br />
+            All Rights Reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+            <Link to='/currencies'>Currencies</Link>
 
+          </Space>
+        </div>
       </div>
     </div>
   )

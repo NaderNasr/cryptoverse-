d51eb538e4e0c1b59e8b2b08cb179c98c4ac61a9
NaderNasr <nadernasr7@gmail.com>
2022-06-07 17:23:57 -0400

    Home page dummy setup

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+
+const { Title } = Typography
+
+
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <Title level={2} className='heading'>
+        Global Crypto Stats
+      </Title>
+      <Row>
+      {/* span 12/24 = half the width of the screen */}
+        <Col>
+          <Statistic span={12} title='totla cypto curr' value='5'/>
+          <Statistic span={12} title='total exchange' value='5'/>
+          <Statistic span={12} title='totla market cap' value='5'/>
+          <Statistic span={12} title='totla 24h volume' value='5'/>
+          <Statistic span={12} title='totla markets' value='5'/>
+        </Col>
+      </Row>
+    </>
   )
 }
 

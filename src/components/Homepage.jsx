e1aa7578334692085b0a1db3b0c0e19d1de415df
NaderNasr <nadernasr7@gmail.com>
2022-06-08 15:41:52 -0400

    showing top 10 currency data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import millify from 'millify';
+import { millify } from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
-
-import {useGetCryptosQuery} from '../services/cryptoAPI'
+import {Currencies, News} from '../components/lib/componentLib'
+import { useGetCryptosQuery } from '../services/cryptoAPI'
 
 const { Title } = Typography
 
@@ -11,9 +11,11 @@ const { Title } = Typography
 
 const Homepage = () => {
 
-  const {data, isFetching} = useGetCryptosQuery()
+  const { data, isFetching } = useGetCryptosQuery(10)
 
   console.log(data)
+  const globalStats = data?.data?.stats;
+  if (isFetching) { return 'loading...' }
 
   return (
     <>
@@ -23,13 +25,24 @@ const Homepage = () => {
       <Row>
         {/* span 12/24 = half the width of the screen */}
         <Col>
-          <Statistic span={12} title='totla cypto curr' value='5' />
-          <Statistic span={12} title='total exchange' value='5' />
-          <Statistic span={12} title='totla market cap' value='5' />
-          <Statistic span={12} title='totla 24h volume' value='5' />
-          <Statistic span={12} title='totla markets' value='5' />
+          <Statistic span={12} title='Total Crypto currency' value={globalStats.total} />
+          <Statistic span={12} title='Total Exchange' value={millify(globalStats.totalExchanges)} />
+          <Statistic span={12} title='Total Market Capital' value={millify(globalStats.totalMarketCap)} />
+          <Statistic span={12} title='Total 24h Volume' value={millify(globalStats.total24hVolume)} />
+          <Statistic span={12} title='Total Markets' value={millify(globalStats.totalMarkets)} />
         </Col>
       </Row>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>Top 10 Crypto Currencies of The World</Title>
+        <Title level={3} className='show-more'><Link to='/currencies'>Show More</Link></Title>
+      </div>
+      <Currencies simplified/>
+
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>Latest Crypto News</Title>
+        <Title level={3} className='show-more'><Link to='/news'>Show More</Link></Title>
+      </div>
+      <News simplified/>
     </>
   )
 }

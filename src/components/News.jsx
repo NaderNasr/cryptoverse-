442a7daa868de040c1a3d5f8886e6d059337ad7b
NaderNasr <nadernasr7@gmail.com>
2022-06-13 12:22:41 -0400

    retrieving data from bing api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Row, Col, Avatar, Select, Typography, Card } from 'antd'
+import moment from 'moment';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
-const News = () => {
+
+const { Text, Title } = Typography
+const { Option } = Select
+
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+  const { data } = useGetCryptoNewsQuery(100);
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+
+  console.log(cryptoNews)
+
   return (
     <div>News</div>
   )

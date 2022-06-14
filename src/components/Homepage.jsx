0b0413f16b93579e2f2669ec0788006ab429f415
NaderNasr <nadernasr7@gmail.com>
2022-06-14 18:05:55 -0400

    fixed spacing and changed from col to row

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -24,16 +24,16 @@ const Homepage = () => {
       </Title>
       <Row>
         {/* span 12/24 = half the width of the screen */}
-        <Col>
-          <Statistic span={12} title='Total Crypto currency' value={globalStats.total} />
-          <Statistic span={12} title='Total Exchange' value={millify(globalStats.totalExchanges)} />
-          <Statistic span={12} title='Total Market Capital' value={millify(globalStats.totalMarketCap)} />
-          <Statistic span={12} title='Total 24h Volume' value={millify(globalStats.total24hVolume)} />
-          <Statistic span={12} title='Total Markets' value={millify(globalStats.totalMarkets)} />
+        <Col style={{display:'flex', flexDirection:'row'}}>
+          <Statistic span={12} title='Total Crypto currency' value={globalStats.total} style={{ padding:'45px'}}/>
+          <Statistic span={12} title='Total Exchange' value={millify(globalStats.totalExchanges)} style={{ padding:'45px'}} />
+          <Statistic span={12} title='Total Market Capital' value={millify(globalStats.totalMarketCap)} style={{ padding:'45px'}} />
+          <Statistic span={12} title='Total 24h Volume' value={millify(globalStats.total24hVolume)} style={{ padding:'45px'}} />
+          <Statistic span={12} title='Total Markets' value={millify(globalStats.totalMarkets)} style={{ padding:'45px'}} />
         </Col>
       </Row>
       <div className='home-heading-container'>
-        <Title level={2} className='home-title'>Top 10 Crypto Currencies of The World</Title>
+        <Title level={2} className='home-title'>Top 5 Crypto Currencies of The World</Title>
         <Title level={3} className='show-more'><Link to='/currencies'>Show More</Link></Title>
       </div>
       <Currencies simplified/>
